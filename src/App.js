6a01372e2869e99192b78841fa9a118334fad56f
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-03-16 17:11:11 +0500

    simple Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAccount, usePrepareContractWrite, useContractWrite, useContractRead } from "wagmi";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {useEffect,useState } from "react";
+import ABI from "./abi.json";
+import useDebounce from "./useDebounce";
+
 
 function App() {
+
+  const { address } = useAccount();
+  const [userBal, setUserBal] = useState(null);
+  const [sendAmount, setSendAmount] = useState(0);
+  const [receiver, setReceiver] = useState("");
+  const [addressToCheck, setAddressToCheck] = useState("");
+  const debouncedAddressToCheck = useDebounce(addressToCheck, 500);
+  const debouncedSendAmount = useDebounce(sendAmount, 500);
+  const debouncedReceiver = useDebounce(receiver, 500);
+
+
+  const { config } = usePrepareContractWrite({
+    address: '0xd0B077908984Cd901945a17358475f3aC1dF6169',
+    abi: ABI,
+    chainId: 80001,
+    functionName: 'transfer',
+    args: [debouncedReceiver, debouncedSendAmount],
+    enabled: Boolean(debouncedSendAmount)
+  });
+
+  const { write } = useContractWrite(config);
+
+  const contractRead = useContractRead({
+    address: '0xd0B077908984Cd901945a17358475f3aC1dF6169',
+    abi: ABI,
+    functionName: 'balanceOf(address)',
+    args: [debouncedAddressToCheck],
+  });
+
+  
+
+  function changeSendAmount(e) {
+    setSendAmount(e.target.value);
+  }
+
+  function changeReceiver(e) {
+    setReceiver(e.target.value);
+  }
+
+  function changeAddressToCheck(e) {
+    setAddressToCheck(e.target.value);
+  }
+  useEffect(() => {
+    if (contractRead.data) {
+      setUserBal(contractRead.data.toString());
+    }
+  }, [contractRead.data]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Send DappToken</h1>
+        <ConnectButton className="App" />
+        <>
+          <h2>Connected Wallet:</h2>
+          <h3>{address}</h3>
+          <input type="number" value={sendAmount} onChange={changeSendAmount} placeholder="Enter amount" />
+          <br />
+          <input type="text" value={receiver} onChange={changeReceiver} placeholder="Enter recipient" />
+          <br />
+          <button disabled={!write} onClick={() => write?.()}>Send</button>
+        </>
+      
+      <h1>Check Balance</h1>
+      <input type="text" value={addressToCheck} onChange={changeAddressToCheck} placeholder="Enter address to check" />
+      <br />
+      {userBal && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Balance of {debouncedAddressToCheck}: {userBal}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@rainbow-me/rainbowkit/styles.css';
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    polygonMumbai
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider 
+      coolMode
+      modalSize='compact'
+      theme={darkTheme({fontStack:'system',overlayBlur:"small"})}
+      chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
